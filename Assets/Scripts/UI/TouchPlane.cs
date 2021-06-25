9bdb1bf30cd2ac7b6e8bf06f8f2d0ac74efbf68d
Anton Korshunov <anton.korshunov@banuba.com>
2021-06-25 15:38:08 +0300

    Folder rename and Touch plane scale fix

--- Assets/Scripts/UI/TouchPlane.cs
+++ Assets/Scripts/UI/TouchPlane.cs
@@ -2,12 +2,24 @@
 
 public class TouchPlane : MonoBehaviour
 {
+    const float TOUCH_PLANE_SCALE = 0.14f;
+
     float worldYPos;
     float worldValueHeight;
+    float newPlaneHeight;
 
+    RectTransform rectTransform;
+    float frustrumHeight;
+    float newPosY;
 
     void Start()
     {
+        rectTransform = GetComponent<RectTransform>();
+        frustrumHeight = (int)Screen.height;
+
+        newPlaneHeight = frustrumHeight * TOUCH_PLANE_SCALE;
+        rectTransform.sizeDelta = new Vector2(rectTransform.sizeDelta.x, newPlaneHeight);
+
         worldYPos = Camera.main.ScreenToWorldPoint(transform.position).y;
         worldValueHeight = Camera.main.ScreenToWorldPoint(new Vector3(GetComponent<RectTransform>().sizeDelta.y,0f)).x;
     }
